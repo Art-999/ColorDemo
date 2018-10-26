4e812a0b6fbeceb472fbb7da13dfe8cc441714cb
arturmusayelyan <musayelyan099@gmail.com>
2018-10-26 13:57:47 +0400

    Initial commit

---
+++ app/src/main/java/com/example/arturmusayelyan/colordemo/colorExample/drawableBackgroungColorChange/DrawableDemo.java
@@ -0,0 +1,15 @@
+package com.example.arturmusayelyan.colordemo.colorExample.drawableBackgroungColorChange;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.arturmusayelyan.colordemo.R;
+
+public class DrawableDemo extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_drawable_demo);
+	}
+}
